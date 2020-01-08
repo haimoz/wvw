7d8ee0e50998224638c271d9715b2168d0ed895a
Haimo Zhang <zh.hammer.dev@gmail.com>
2020-01-08 21:53:35 +1300

    added a utility function to designate serial port

--- wvw/utils.py
+++ wvw/utils.py
@@ -1,3 +1,7 @@
+import serial
+from serial.tools.list_ports import comports
+
+
 def boundaries_of(x):
     """
     Given a sequence of central values, calculate its boundaries.
@@ -18,3 +22,30 @@ def boundaries_of(x):
                 "Boundaries can only be calculated for a sequence type"
                 " that minimally should support sorting, length, and indexing."
                 "  Received " + repr(type(x)) + " instead!")
+
+
+def get_serial(port, *args, **kwargs):
+    """
+    A wrapper method for opening serial port with automatic port listing and
+    automatic/interactive selection.
+    """
+    if port is not None:
+        return serial.Serial(port, *args, **kwargs)
+    else:
+        ports = tuple(comports())  # list detected serial ports
+    if len(ports) == 0:
+        raise Exception("No serial ports found!")
+    elif len(ports) == 1:
+        port = ports[0]
+    else:
+        port = None
+        while port is None:
+            sel = int(input(
+                "\nAvailable ports:\n{}\n\nSelect port to use: ".format(
+                    '\n'.join([
+                        str(i) + ": " + x.device
+                        for i, x in enumerate(ports)]))))
+            if sel >= 0 and sel < len(ports):
+                port = ports[sel]
+    print("\nUsing port: {}".format(port.device))
+    return serial.Serial(port, *args, **kwargs)
