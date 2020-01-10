5295d88f1976b7f90af1f95a1c6aae1d04ee574c
Haimo Zhang <zh.hammer.dev@gmail.com>
2020-01-10 16:52:36 +1300

    updated demo script and generated more meaningful mock data

--- examples/spectrometer.py
+++ examples/spectrometer.py
@@ -1,25 +1,38 @@
 from wvw.devices import Spectrometer
 from random import randint
+import math
 
 
 if __name__ == '__main__':
 
+    # mock data
     def r(n):
         while 1:
             yield tuple(randint(0, 1000) / 1000 for x in range(n))
+    def r2(n):
+        t = 0
+        while 1:
+            t += 0.01
+            f = math.sin(t)
+            yield tuple(math.sin(f*t) for x in range(n))
 
-    NUM_CH = 2
+    # number of sensor input
+    NUM_CH = 1
 
+    # initialization of a spectrometer
     s = Spectrometer(
             number_of_channels=NUM_CH,
             spectrum_init_params={
-                'window_size'     : 32,
-                'window_stride'   : 32,
-                'history_size'    : 50,
+                'window_size'     : 500,
+                'window_stride'   : 500,
+                'history_size'    : 1000,
                 'fixed_data_rate' : None,
-            }
+            },
+            fps=5
         )
 
-    for data in r(NUM_CH):
+    # sending data to the spectrometer for FFT and visualization
+    # the input data needs to be a tuple of length NUM_CH
+    for data in r2(NUM_CH):
         if s.update(data):
             s.draw()
